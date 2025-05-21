b826a79ba60e1b70a9554cc0bb09aed4d3ec8384
zuhair2808 <zuhair2808@gmail.com>
2025-05-21 20:58:25 +0600

    lesson 14

---
+++ lesson 14/script.js
@@ -0,0 +1,11 @@
+let historyValue= document.getElementById("history-value");
+let outputValue = document.getElementById("output-value");
+
+let clearButton = document.getElementById("clear");
+let backspaceButton = document.getElementById("backspace");
+let equalsButton = document.getElementById("=");
+
+let currentInput="";
+let previousInput = "";
+let operator = "";
+let newCalculation = false;
